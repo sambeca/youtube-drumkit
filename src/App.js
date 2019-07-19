d0e3a57859986146697dbd512cf4c2939affe1b9
Samuel Beca <samuel.beca@gmail.com>
2019-07-19 15:49:32 +0200

    add seach bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import SearchBar from "./components/search_bar";
 
-const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 class App extends Component {
   render() {
-    return <div>Youtube Drumkit</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
