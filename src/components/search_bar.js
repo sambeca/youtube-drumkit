7939a357628bef7df007de7abbbe4d19f4e52bca
Samuel Beca <samuel.beca@gmail.com>
2019-07-19 15:51:33 +0200

    add search bar logic

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+    this.props.onSearchTermChange(event.target.value);
+  }
+
   render() {
-    return <input />;
+    return (
+      <div className="search-bar">
+        <input value={this.state.term} onChange={this.onInputChange} />
+      </div>
+    );
   }
 }
 
